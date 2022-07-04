5aef9c6ce9c96c95e52cc2b0aa9a4293cae82e6e
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-04 15:05:18 +0800

    ✨ Added input component

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Input.css";
+
+type Props = {
+  placeholder: string;
+  name: string;
+  type: string;
+  value?: string;
+  handleChange: (event: React.ChangeEvent, name: string) => void;
+};
+
+function Input({
+  placeholder,
+  name,
+  type,
+  value,
+  handleChange,
+  ...props
+}: Props) {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(event) => handleChange(event, name)}
+      className="input"
+      {...props}
+    />
+  );
+}
+
+export default Input;
