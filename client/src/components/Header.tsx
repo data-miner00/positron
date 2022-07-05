4918586d526b1c08eaeb01ec25983cd3c4e04745
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-05 22:19:09 +0800

    ✨ Added header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,46 @@
+import { RiMoonClearFill } from "react-icons/ri";
+import { AiFillGithub } from "react-icons/ai";
+import AccountInfo from "./AccountInfo";
+import ImgButton from "./ImgButton";
+import "./Header.css";
+
 function Header() {
-  return <></>;
+  return (
+    <div className="header-wrapper">
+      <header className="header">
+        <div className="logo">
+          <span>Positron+</span>
+        </div>
+
+        <div className="right">
+          <nav className="nav">
+            <ul>
+              <li>
+                <a className="link" href="#">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a className="link" href="#">
+                  Explorer
+                </a>
+              </li>
+              {/* <li>
+                <a href="#">
+                  <ImgButton img={<AiFillGithub />} />
+                </a>
+              </li> */}
+              <li>
+                <ImgButton img={<RiMoonClearFill />} />
+              </li>
+            </ul>
+          </nav>
+          <div className="divider"></div>
+          <AccountInfo />
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
