fb987246b3fcbccb53a48bc0760804242d6addbd
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-05 22:20:23 +0800

    ✨ Revamped form

--- client/src/components/TransferForm.tsx
+++ client/src/components/TransferForm.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Button from "./Button";
+import Input from "./Input";
+
+import "./TransferForm.css";
 
 function TransferForm() {
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -7,12 +11,34 @@ function TransferForm() {
   }
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input type="text" placeholder="Address To" />
-      <input type="text" placeholder="Amount (ETH)" />
-      <input type="text" placeholder="Keyword (GIF)" />
-      <input type="text" placeholder="Enter Message" />
-      <button>Send Now</button>
+    <form onSubmit={(e) => handleSubmit(e)} className="transfer-form">
+      <h1>Who do you want to send today?</h1>
+      <Input
+        placeholder="Address To"
+        type="text"
+        name="addressTo"
+        handleChange={() => {}}
+      />
+      <Input
+        placeholder="Amount (ETH)"
+        type="text"
+        name="amount"
+        handleChange={() => {}}
+      />
+      <Input
+        placeholder="Keyword"
+        type="text"
+        name="keyword"
+        handleChange={() => {}}
+      />
+      <Input
+        placeholder="Enter Message"
+        type="text"
+        name="message"
+        handleChange={() => {}}
+      />
+
+      <Button type="submit" primary label="Deliver Now" />
     </form>
   );
 }
