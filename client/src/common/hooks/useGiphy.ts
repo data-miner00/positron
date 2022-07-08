467b935426ef28f2a93dbc77eb8761aefba3dd0e
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-08 23:15:01 +0800

    ✨ Added useGiphy hook

---
+++ client/src/common/hooks/useGiphy.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const GIPHY_SEARCH_URL = "https://api.giphy.com/v1/gifs/search";
+
+function useGiphy(keyword: string) {
+  const [gifUrl, setGifUrl] = useState("");
+
+  async function fetchGifs() {
+    try {
+      const response = await fetch(
+        GIPHY_SEARCH_URL +
+          `?api_key=${GIPHY_API_KEY}&q=${keyword.split(" ").join("")}&limit=1`
+      );
+      const { data } = await response.json();
+
+      setGifUrl(data[0]?.images?.downsized_medium?.url);
+    } catch (error: unknown) {
+      console.log(error);
+      // default gif
+      setGifUrl(
+        "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"
+      );
+    }
+  }
+
+  useEffect(() => {
+    if (keyword) fetchGifs();
+  }, [keyword]);
+
+  return gifUrl;
+}
+
+export default useGiphy;
