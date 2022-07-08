5827e4231b395eaaffbddc75a455a512016e4cc8
data-miner00 <data-miner00@users.noreply.github.com>
2022-07-08 09:08:12 +0800

    ✨ Added chars to show in shortenAdress

--- client/src/utils/shortenAddress.ts
+++ client/src/utils/shortenAddress.ts
@@ -1,8 +1,8 @@
-export function shortenAddress(address: string) {
+export function shortenAddress(address: string, showLength: number = 4) {
   const lastIndex = address.length;
   return (
-    address.substring(0, 6) +
+    address.substring(0, showLength + 2) +
     "..." +
-    address.substring(lastIndex - 4, lastIndex)
+    address.substring(lastIndex - showLength, lastIndex)
   );
 }
