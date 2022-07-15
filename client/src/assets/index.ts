a1c6b0712146663d6a703dea5bfe24ac1f2de61f
Shaun Chong <mumk0313@gmail.com>
2022-07-15 22:18:57 +0800

    🍱 Move img to assets and re-export

---
+++ client/src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as cardano } from "./images/cardano.png";
+export { default as ens } from "./images/ens.png";
+export { default as chainlink } from "./images/link.png";
+export { default as polygon } from "./images/matic.png";
+export { default as oneinch } from "./images/1inch.png";
+export { default as ren } from "./images/ren.png";
