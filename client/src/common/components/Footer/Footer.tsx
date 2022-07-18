9884dce045f204406c5cdf377da47ed7cccf42e9
Shaun Chong <mumk0313@gmail.com>
2022-07-18 23:29:33 +0800

    🚧 Added temp footer

---
+++ client/src/common/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="l">
+        <p>Made with ❤️ by Shaun.</p>
+      </div>
+      <ul className="links">
+        <li></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
