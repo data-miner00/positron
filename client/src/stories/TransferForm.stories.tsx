93dab1a7a3b4377c8830683a719fbc40e3fb43a8
Shaun Chong <mumk0313@gmail.com>
2022-07-20 21:40:11 +0800

    🔥 Remove leftover file

--- client/src/stories/TransferForm.stories.tsx
+++
@@ -1,8 +0,0 @@
-import TransferForm from "../components/TransferForm";
-
-export default {
-  title: "Fund Transfer Form",
-  component: TransferForm,
-};
-
-export const Normal = () => <TransferForm></TransferForm>;
