2da15c1a73f3e41de20ba88595478d9a23ef07ef
Shaun Chong <mumk0313@gmail.com>
2022-07-20 22:30:24 +0800

    🚨 Fix currentAccount not found

--- client/src/pages/home/components/TransferForm/TransferForm.stories.tsx
+++ client/src/pages/home/components/TransferForm/TransferForm.stories.tsx
@@ -1,8 +1,19 @@
+import { ComponentMeta } from "@storybook/react";
+import { AppContext } from "setup/app-context-manager/AppContext";
+import { withReactContext } from "storybook-react-context";
 import TransferForm from "./TransferForm";
 
 export default {
   title: "Fund Transfer Form",
   component: TransferForm,
-};
+  decorators: [
+    withReactContext({
+      Context: AppContext,
+      initialState: {
+        currentAccount: "0x430EF9263E06DAE63c84292C3409D61c598E9682",
+      },
+    }),
+  ],
+} as ComponentMeta<typeof TransferForm>;
 
-export const Normal = () => <TransferForm></TransferForm>;
+export const Normal = () => <TransferForm />;
