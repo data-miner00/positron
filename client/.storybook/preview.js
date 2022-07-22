78c44fa436e358dd495415168c798b3e15d075f5
Shaun Chong <mumk0313@gmail.com>
2022-07-22 08:57:49 +0800

    🚑 Fix Link cannot be used in Storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,9 @@
+import { addDecorator } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
+
 import "../src/App.css";
+
+addDecorator((story) => <BrowserRouter>{story()}</BrowserRouter>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
