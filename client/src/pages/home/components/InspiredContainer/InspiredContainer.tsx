dee9eea41569dcf9afae5d1f26eee72f8b160e51
Shaun Chong <mumk0313@gmail.com>
2022-07-22 09:00:59 +0800

    🚨 Added key and noreferrer

--- client/src/pages/home/components/InspiredContainer/InspiredContainer.tsx
+++ client/src/pages/home/components/InspiredContainer/InspiredContainer.tsx
@@ -4,9 +4,9 @@ import "./InspiredContainer.css";
 function InspiredContainer({ inspirings }: InspiredContainerProps) {
   return (
     <ul className="inspired-container">
-      {inspirings.map((inspire) => (
-        <li>
-          <a href={inspire.link} target="_blank">
+      {inspirings.map((inspire, index) => (
+        <li key={index}>
+          <a href={inspire.link} target="_blank" rel="noreferrer">
             <img src={inspire.image} alt={inspire.name} />
           </a>
         </li>
