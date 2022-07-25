6adfa9705c0a07a6875bb1ffecf7d272b3c595f4
Shaun Chong <mumk0313@gmail.com>
2022-07-26 01:18:14 +0800

    ✨ Fetching real data from blockchain

--- client/src/pages/home/components/TransactionsContainer/index.tsx
+++ client/src/pages/home/components/TransactionsContainer/index.tsx
@@ -1,19 +1,52 @@
+import { useEffect, useState } from "react";
+import { Transaction } from "setup/app-context-manager/models";
+import { getAllTransactionsAsync } from "setup/app-context-manager/utils";
 import { formatTimeDiff } from "../../utils";
 import TransactionCard from "../TransactionCard";
 
 import "./styles.css";
 
+const { ethereum } = window;
+
 function TransactionsContainer() {
+  const [transactions, setTransactions] = useState<Array<Transaction>>();
+
+  useEffect(() => {
+    getAllTransactionsAsync(ethereum)
+      .then((txs) => txs && setTransactions(txs))
+      .catch(console.error);
+  }, []);
+
+  /* v Temporary codes */
+  if (!transactions) {
+    return (
+      <div className="transactions-container">
+        {Array(4).map(() => (
+          <TransactionCard
+            addressTo="0x4392805C28f47d334856D33d62F8ec539933478D"
+            addressFrom="0x4392805C28f47d334856D33d62F8ec539933478D"
+            timestamp={formatTimeDiff(new Date(2022, 4, 21))}
+            message="hello world"
+            keyword="hello"
+            amount="0.1"
+          />
+        ))}
+      </div>
+    );
+  }
+  /* ^ Temporary codes */
+
   return (
     <div className="transactions-container">
-      {[1, 2, 2, 2].map(() => (
+      {transactions?.map((tx, index) => (
         <TransactionCard
-          addressTo="0x4392805C28f47d334856D33d62F8ec539933478D"
-          addressFrom="0x4392805C28f47d334856D33d62F8ec539933478D"
-          timestamp={formatTimeDiff(new Date(2022, 4, 21))}
-          message="hello world"
-          keyword="hello"
-          amount="0.1"
+          key={index}
+          addressTo={tx.addressTo}
+          addressFrom={tx.addressFrom}
+          timestamp={formatTimeDiff(tx.timestamp)}
+          message={tx.message}
+          keyword={tx.keyword}
+          amount={tx.amount}
         />
       ))}
     </div>
