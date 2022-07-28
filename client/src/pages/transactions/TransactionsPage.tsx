dbcd3ac015bb9318916477e9bb9fe8397a111533
Shaun Chong <mumk0313@gmail.com>
2022-07-28 22:25:51 +0800

    ✨ Added transactions page

---
+++ client/src/pages/transactions/TransactionsPage.tsx
@@ -0,0 +1,23 @@
+import TransactionsContainer from "pages/home/components/TransactionsContainer";
+import { useContext } from "react";
+import { TransactionsContext } from "setup/app-context-manager/TransactionsContext";
+import "./TransactionsPage.css";
+
+function TransactionsPage() {
+  const { transactions } = useContext(TransactionsContext);
+
+  return (
+    <div className="transactions-page">
+      <section className="intro">
+        <h1>Transactions Masterlist</h1>
+        <p>Explore the entire transactions that happened on here.</p>
+      </section>
+
+      <section className="transactions">
+        <TransactionsContainer transactions={transactions} />
+      </section>
+    </div>
+  );
+}
+
+export default TransactionsPage;
--- client/src/setup/routes-manager/index.tsx
+++ client/src/setup/routes-manager/index.tsx
@@ -1,10 +1,13 @@
+import TransactionsPage from "pages/transactions";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "../../pages/home"
+import HomePage from "../../pages/home";
 
-const Routing = () =>
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-    </Routes>
+const Routing = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/transactions" element={<TransactionsPage />} />
+  </Routes>
+);
 
 export default Routing;
