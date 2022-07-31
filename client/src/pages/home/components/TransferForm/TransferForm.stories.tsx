aee7395458640c6a5e8b6288709f4724392a371e
Shaun Chong <mumk0313@gmail.com>
2022-07-31 23:29:16 +0800

    📝 Fix transfer form storybook context error

--- client/src/pages/home/components/TransferForm/TransferForm.stories.tsx
+++ client/src/pages/home/components/TransferForm/TransferForm.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta } from "@storybook/react";
 import { AppContext } from "setup/app-context-manager/AppContext";
+import { TransactionsContext } from "setup/app-context-manager/TransactionsContext";
 import { withReactContext } from "storybook-react-context";
 import TransferForm from "./TransferForm";
 
@@ -11,6 +12,12 @@ export default {
       Context: AppContext,
       initialState: {
         currentAccount: "0x430EF9263E06DAE63c84292C3409D61c598E9682",
+      },
+    }),
+    withReactContext({
+      Context: TransactionsContext,
+      initialState: {
+        transactions: [],
       },
     }),
   ],
