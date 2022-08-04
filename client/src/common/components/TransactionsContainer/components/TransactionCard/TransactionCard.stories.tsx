ea37c3c1ccf143a3814edb6526396e5e05c57a9d
Shaun Chong <mumk0313@gmail.com>
2022-08-04 22:36:49 +0800

    📝 Added TransactionCard story

---
+++ client/src/common/components/TransactionsContainer/components/TransactionCard/TransactionCard.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { TransactionCardProps } from "common/components/models";
+
+import TransactionCard from "./TransactionCard";
+
+export default {
+  title: "Transaction Card",
+  component: TransactionCard,
+} as ComponentMeta<typeof TransactionCard>;
+
+const Template = (args: TransactionCardProps) => <TransactionCard {...args} />;
+
+export const Default: ComponentStory<typeof TransactionCard> = Template.bind(
+  {}
+);
+Default.args = {
+  addressFrom: "0x1234567890qweeryuioop",
+  addressTo: "0x6666667890qweeryuioop",
+  timestamp: "2 weeks ago",
+  message: "hello world",
+  keyword: "sand",
+  amount: "1.56",
+};
