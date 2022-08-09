83f59808bd1849d2a8109c6cbd5c90a489fd33b5
Shaun Chong <mumk0313@gmail.com>
2022-08-09 19:01:45 +0800

    ✅ Add button attribute test

--- client/src/common/components/Button/Button.spec.tsx
+++ client/src/common/components/Button/Button.spec.tsx
@@ -5,7 +5,23 @@ describe("Button", () => {
   it("should render the button text provided", () => {
     const testId = "button";
     const textContent = "Testing button";
+
     render(<Button label={textContent} data-testid={testId} />);
-    expect(screen.getByTestId(testId)).toHaveTextContent(textContent);
+
+    const buttonElement = screen.getByTestId(testId);
+
+    expect(buttonElement).toHaveTextContent(textContent);
+  });
+
+  it("should render the correct attributes", () => {
+    const testId = "button";
+    const textContent = "Testing button";
+
+    render(<Button label={textContent} data-testid={testId} />);
+
+    const buttonElement = screen.getByTestId(testId);
+
+    expect(buttonElement).toHaveClass("button", "normal");
+    expect(buttonElement).toHaveAttribute("type", "button");
   });
 });
