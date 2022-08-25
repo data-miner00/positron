13c2ab286a7565d79812abba316676cdd322c56e
Shaun Chong <mumk0313@gmail.com>
2022-08-25 21:53:59 +0800

    💄 Added motion and list tag

--- client/src/common/components/TransactionsContainer/TransactionsContainer.tsx
+++ client/src/common/components/TransactionsContainer/TransactionsContainer.tsx
@@ -1,5 +1,6 @@
-import { Transaction } from "setup/app-context-manager/models";
+import { motion } from "framer-motion";
 
+import { Transaction } from "setup/app-context-manager/models";
 import { formatTimeDiff } from "pages/home/utils";
 import TransactionCard from "./components/TransactionCard";
 
@@ -18,19 +19,23 @@ function TransactionsContainer({
   if (transactions.length < 1) {
     return (
       <div className="transactions-container">
-        {Array(6)
-          .fill(undefined)
-          .map((_, index) => (
-            <TransactionCard
-              key={index}
-              addressTo="0x4392805C28f47d334856D33d62F8ec539933478D"
-              addressFrom="0x4392805C28f47d334856D33d62F8ec539933478D"
-              timestamp={formatTimeDiff(new Date(2022, 4, 21))}
-              message="hello world"
-              keyword="hello"
-              amount="0.1"
-            />
-          ))}
+        <ul>
+          {Array(6)
+            .fill(undefined)
+            .map((_, index) => (
+              <li>
+                <TransactionCard
+                  key={index}
+                  addressTo="0x4392805C28f47d334856D33d62F8ec539933478D"
+                  addressFrom="0x4392805C28f47d334856D33d62F8ec539933478D"
+                  timestamp={formatTimeDiff(new Date(2022, 4, 21))}
+                  message="hello world"
+                  keyword="hello"
+                  amount="0.1"
+                />
+              </li>
+            ))}
+        </ul>
       </div>
     );
   }
@@ -39,34 +44,46 @@ function TransactionsContainer({
   if (limit > 0) {
     return (
       <div className="transactions-container">
-        {transactions?.slice(0, limit).map((tx, index) => (
-          <TransactionCard
-            key={index}
-            addressTo={tx.addressTo}
-            addressFrom={tx.addressFrom}
-            timestamp={formatTimeDiff(tx.timestamp)}
-            message={tx.message}
-            keyword={tx.keyword}
-            amount={tx.amount}
-          />
-        ))}
+        <ul>
+          {transactions?.slice(0, limit).map((tx, index) => (
+            <li>
+              <TransactionCard
+                key={index}
+                addressTo={tx.addressTo}
+                addressFrom={tx.addressFrom}
+                timestamp={formatTimeDiff(tx.timestamp)}
+                message={tx.message}
+                keyword={tx.keyword}
+                amount={tx.amount}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 
   return (
     <div className="transactions-container">
-      {transactions?.map((tx, index) => (
-        <TransactionCard
-          key={index}
-          addressTo={tx.addressTo}
-          addressFrom={tx.addressFrom}
-          timestamp={formatTimeDiff(tx.timestamp)}
-          message={tx.message}
-          keyword={tx.keyword}
-          amount={tx.amount}
-        />
-      ))}
+      <ul>
+        {transactions?.map((tx, index) => (
+          <motion.li
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.2, delay: (index + 1) * 0.2 }}
+          >
+            <TransactionCard
+              key={index}
+              addressTo={tx.addressTo}
+              addressFrom={tx.addressFrom}
+              timestamp={formatTimeDiff(tx.timestamp)}
+              message={tx.message}
+              keyword={tx.keyword}
+              amount={tx.amount}
+            />
+          </motion.li>
+        ))}
+      </ul>
     </div>
   );
 }
