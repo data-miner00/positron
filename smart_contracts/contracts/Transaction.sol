37326b769cb235cc1038bd4531b3b3ef262b8e9a
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-21 20:50:31 +0800

    🔥 Remove empty constructor

--- smart_contracts/contracts/Transaction.sol
+++ smart_contracts/contracts/Transaction.sol
@@ -4,9 +4,6 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Transaction {
     uint256 transactionCounter;
 
-    constructor() public {
-    }
-
     event Transfer(address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
     struct TransferDetails {
