6a8b4f2ddcf475b8e1695614db55dfdcf9fc4dcc
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-22 00:31:56 +0800

    💄 Fix padding footer

--- client/src/common/components/Footer/Footer.tsx
+++ client/src/common/components/Footer/Footer.tsx
@@ -2,11 +2,13 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer role="footer">
-      <div className="l">
-        <p>Made with ❤️ by Shaun in Giällewee, L-9749 Clervaux</p>
-      </div>
-    </footer>
+    <div className="fwrapper">
+      <footer role="footer">
+        <div className="l">
+          <p>Made with ❤️ by Shaun in Giällewee, L-9749 Clervaux</p>
+        </div>
+      </footer>
+    </div>
   );
 }
 
