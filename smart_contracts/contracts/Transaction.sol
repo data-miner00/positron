a2fbcda3578c5c316c042ac8ca25bd04ed4aef8d
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-21 23:04:27 +0800

    ✨ Add more states and reader

--- smart_contracts/contracts/Transaction.sol
+++ smart_contracts/contracts/Transaction.sol
@@ -2,10 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Transaction {
-    uint256 transactionCounter;
-
-    event Transfer(address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-
     struct TransferDetails {
         address sender;
         address receiver;
@@ -16,9 +12,22 @@ contract Transaction {
     }
 
     TransferDetails[] transactions;
+    uint256 transactionCounter;
+    uint256 totalVolume;
+
+    mapping(address => uint256) public accountTxCounts;
+    mapping(address => uint256) public accountSendVolumes;
+    mapping(address => uint256) public accountReceiveVolumes;
+
+    event Transfer(address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
     function commit(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCounter++;
+        totalVolume += amount;
+        accountTxCounts[msg.sender]++;
+        accountSendVolumes[msg.sender] += amount;
+        accountReceiveVolumes[receiver] += amount;
+
         transactions.push(TransferDetails(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
@@ -30,5 +39,27 @@ contract Transaction {
 
     function getTransactionCount() public view returns (uint256) {
         return transactionCounter;
+    }
+
+    function getTotalVolume() public view returns (uint256) {
+        return totalVolume;
+    }
+
+    function getRank() public view returns (string memory) {
+        uint sentAmount = accountSendVolumes[msg.sender];
+
+        if (sentAmount > 30 ether) {
+            return "S+";
+        } else if (sentAmount > 10 ether) {
+            return "S";
+        } else if (sentAmount > 5 ether) {
+            return "A";
+        } else if (sentAmount > 1 ether) {
+            return "B";
+        } else if (sentAmount > 0) {
+            return "C";
+        } else {
+            return "D";
+        }
     }
 }
